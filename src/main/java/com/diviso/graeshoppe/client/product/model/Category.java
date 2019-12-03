a50a57f46f70e192597803242fd4f3aa94c23251
sruthykc <sruthy.k.c@gmail.com>
2019-12-03 11:21:49 +0530

    category fix

--- src/main/java/com/diviso/graeshoppe/client/product/model/Category.java
+++ src/main/java/com/diviso/graeshoppe/client/product/model/Category.java
@@ -1,17 +1,30 @@
 package com.diviso.graeshoppe.client.product.model;
 
 import java.util.Objects;
+import java.util.Set;
+
 import com.diviso.graeshoppe.client.product.model.Product;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
 
@@ -22,272 +35,172 @@ import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-10-28T15:55:43.394+05:30[Asia/Kolkata]")
 @Document(indexName = "category")
 public class Category   {
-  @JsonProperty("description")
-  private String description = null;
+	  private static final long serialVersionUID = 1L;
+	    
 
-  public String getImageLink() {
-	return imageLink;
-}
+	    private Long id;
 
-public void setImageLink(String imageLink) {
-	this.imageLink = imageLink;
-}
+	    private String iDPcode;
 
-@JsonProperty("iDPcode")
-  private String iDPcode = null;
+	    private String name;
 
-  @JsonProperty("id")
-  private Long id = null;
-  
-  
-  @JsonProperty("imagelink")
-  private String imageLink = null;
+	    private byte[] image;
 
-  @JsonProperty("image")
-  private byte[] image = null;
+	    private String imageContentType;
 
-  @JsonProperty("imageContentType")
-  private String imageContentType = null;
+	    private String imageLink;
 
-  @JsonProperty("name")
-  private String name = null;
+	    private String description;
 
-  @JsonProperty("products")
-  @Valid
-  private List<Product> products = null;
+	    @OneToMany(mappedBy = "category")
+	    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+	    private Set<Product> products = new HashSet<>();
+	    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+	    public Long getId() {
+	        return id;
+	    }
 
-  public Category description(String description) {
-    this.description = description;
-    return this;
-  }
+	    public void setId(Long id) {
+	        this.id = id;
+	    }
 
-  /**
-   * Get description
-   * @return description
-  **/
-  @ApiModelProperty(value = "")
+	    public String getiDPcode() {
+	        return iDPcode;
+	    }
 
+	    public Category iDPcode(String iDPcode) {
+	        this.iDPcode = iDPcode;
+	        return this;
+	    }
 
-  public String getDescription() {
-    return description;
-  }
+	    public void setiDPcode(String iDPcode) {
+	        this.iDPcode = iDPcode;
+	    }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+	    public String getName() {
+	        return name;
+	    }
 
-  public Category iDPcode(String iDPcode) {
-    this.iDPcode = iDPcode;
-    return this;
-  }
+	    public Category name(String name) {
+	        this.name = name;
+	        return this;
+	    }
 
-  /**
-   * Get iDPcode
-   * @return iDPcode
-  **/
-  @ApiModelProperty(value = "")
+	    public void setName(String name) {
+	        this.name = name;
+	    }
 
+	    @ApiModelProperty(value = "")
 
-  public String getIDPcode() {
-    return iDPcode;
-  }
+	    @Pattern(regexp="^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$") 
+	      public byte[] getImage() {
+	        return image;
+	      }
 
-  public void setIDPcode(String iDPcode) {
-    this.iDPcode = iDPcode;
-  }
+	    public Category image(byte[] image) {
+	        this.image = image;
+	        return this;
+	    }
 
-  public Category id(Long id) {
-    this.id = id;
-    return this;
-  }
+	    public void setImage(byte[] image) {
+	        this.image = image;
+	    }
 
-  /**
-   * Get id
-   * @return id
-  **/
-  @ApiModelProperty(value = "")
+	    public String getImageContentType() {
+	        return imageContentType;
+	    }
 
+	    public Category imageContentType(String imageContentType) {
+	        this.imageContentType = imageContentType;
+	        return this;
+	    }
 
-  public Long getId() {
-    return id;
-  }
+	    public void setImageContentType(String imageContentType) {
+	        this.imageContentType = imageContentType;
+	    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	    public String getImageLink() {
+	        return imageLink;
+	    }
 
-  public Category image(byte[] image) {
-    this.image = image;
-    return this;
-  }
+	    public Category imageLink(String imageLink) {
+	        this.imageLink = imageLink;
+	        return this;
+	    }
 
-  /**
-   * Get image
-   * @return image
-  **/
-  @ApiModelProperty(value = "")
+	    public void setImageLink(String imageLink) {
+	        this.imageLink = imageLink;
+	    }
 
-@Pattern(regexp="^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$") 
-  public byte[] getImage() {
-    return image;
-  }
+	    public String getDescription() {
+	        return description;
+	    }
 
-  public void setImage(byte[] image) {
-    this.image = image;
-  }
+	    public Category description(String description) {
+	        this.description = description;
+	        return this;
+	    }
 
-  public Category imageContentType(String imageContentType) {
-    this.imageContentType = imageContentType;
-    return this;
-  }
+	    public void setDescription(String description) {
+	        this.description = description;
+	    }
 
-  /**
-   * Get imageContentType
-   * @return imageContentType
-  **/
-  @ApiModelProperty(value = "")
+	    public Set<Product> getProducts() {
+	        return products;
+	    }
 
+	    public Category products(Set<Product> products) {
+	        this.products = products;
+	        return this;
+	    }
 
-  public String getImageContentType() {
-    return imageContentType;
-  }
+	    public Category addProducts(Product product) {
+	        this.products.add(product);
+	        product.setCategory(this);
+	        return this;
+	    }
 
-  public void setImageContentType(String imageContentType) {
-    this.imageContentType = imageContentType;
-  }
+	    public Category removeProducts(Product product) {
+	        this.products.remove(product);
+	        product.setCategory(null);
+	        return this;
+	    }
 
-  public Category name(String name) {
-    this.name = name;
-    return this;
-  }
+	    public void setProducts(Set<Product> products) {
+	        this.products = products;
+	    }
+	    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
 
-  /**
-   * Get name
-   * @return name
-  **/
-  @ApiModelProperty(value = "")
+	    @Override
+	    public boolean equals(Object o) {
+	        if (this == o) {
+	            return true;
+	        }
+	        if (o == null || getClass() != o.getClass()) {
+	            return false;
+	        }
+	        Category category = (Category) o;
+	        if (category.getId() == null || getId() == null) {
+	            return false;
+	        }
+	        return Objects.equals(getId(), category.getId());
+	    }
 
+	    @Override
+	    public int hashCode() {
+	        return Objects.hashCode(getId());
+	    }
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Category products(List<Product> products) {
-    this.products = products;
-    return this;
-  }
-
-  public Category addProductsItem(Product productsItem) {
-    if (this.products == null) {
-      this.products = new ArrayList<Product>();
-    }
-    this.products.add(productsItem);
-    return this;
-  }
-
-  /**
-   * Get products
-   * @return products
-  **/
-  @ApiModelProperty(value = "")
-
-  @Valid
-
-  public List<Product> getProducts() {
-    return products;
-  }
-
-  public void setProducts(List<Product> products) {
-    this.products = products;
-  }
-
-
-
-
-  @Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((description == null) ? 0 : description.hashCode());
-	result = prime * result + ((iDPcode == null) ? 0 : iDPcode.hashCode());
-	result = prime * result + ((id == null) ? 0 : id.hashCode());
-	result = prime * result + Arrays.hashCode(image);
-	result = prime * result + ((imageContentType == null) ? 0 : imageContentType.hashCode());
-	result = prime * result + ((imageLink == null) ? 0 : imageLink.hashCode());
-	result = prime * result + ((name == null) ? 0 : name.hashCode());
-	result = prime * result + ((products == null) ? 0 : products.hashCode());
-	return result;
-}
-
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Category other = (Category) obj;
-	if (description == null) {
-		if (other.description != null)
-			return false;
-	} else if (!description.equals(other.description))
-		return false;
-	if (iDPcode == null) {
-		if (other.iDPcode != null)
-			return false;
-	} else if (!iDPcode.equals(other.iDPcode))
-		return false;
-	if (id == null) {
-		if (other.id != null)
-			return false;
-	} else if (!id.equals(other.id))
-		return false;
-	if (!Arrays.equals(image, other.image))
-		return false;
-	if (imageContentType == null) {
-		if (other.imageContentType != null)
-			return false;
-	} else if (!imageContentType.equals(other.imageContentType))
-		return false;
-	if (imageLink == null) {
-		if (other.imageLink != null)
-			return false;
-	} else if (!imageLink.equals(other.imageLink))
-		return false;
-	if (name == null) {
-		if (other.name != null)
-			return false;
-	} else if (!name.equals(other.name))
-		return false;
-	if (products == null) {
-		if (other.products != null)
-			return false;
-	} else if (!products.equals(other.products))
-		return false;
-	return true;
-}
-
-@Override
-public String toString() {
-	return "Category [description=" + description + ", iDPcode=" + iDPcode + ", id=" + id + ", imageLink=" + imageLink
-			+ ", image=" + Arrays.toString(image) + ", imageContentType=" + imageContentType + ", name=" + name
-			+ ", products=" + products + "]";
-}
-
-/**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
-}
-
+	    @Override
+	    public String toString() {
+	        return "Category{" +
+	            "id=" + getId() +
+	            ", iDPcode='" + getiDPcode() + "'" +
+	            ", name='" + getName() + "'" +
+	            ", image='" + getImage() + "'" +
+	            ", imageContentType='" + getImageContentType() + "'" +
+	            ", imageLink='" + getImageLink() + "'" +
+	            ", description='" + getDescription() + "'" +
+	            "}";
+	    }
+	}
