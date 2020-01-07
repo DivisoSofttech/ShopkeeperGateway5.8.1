fd7ac084112ff9d54efbe17f0b81d39f7c6d4131
abilash-diviso <abilash.s@lxisoft.com>
2020-01-07 14:13:59 +0530

    order swaggered

--- src/main/java/undefined/client/order/api/NotificationResourceApiClient.java
+++
@@ -1,8 +0,0 @@
-package undefined.client.order.api;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import undefined.client.order.ClientConfiguration;
-
-@FeignClient(name="${order.name:order}", url="${order.url:dev.ci2.divisosofttech.com:8088/}", configuration = ClientConfiguration.class)
-public interface NotificationResourceApiClient extends NotificationResourceApi {
-}
